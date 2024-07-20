a5639313c2e83c3ec53050d3b42605b0f7852332
Eli Keli <muthokaeli@gmail.com>
2024-07-20 15:32:58 +0300

    Modified script file

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
         removeButton.onclick = () => {
             taskList.removeChild(listElement);
         };
